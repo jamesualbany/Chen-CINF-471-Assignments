f6bcc7c8885c099bc2d773bf27a10c8e3548d43f
jamesualbany <jchen61@albany.edu>
2025-01-30 12:49:01 -0500

    RollerBall
    rollerball demo

---
+++ 471 project/Assets/RollerBall/testing.cs
@@ -0,0 +1,29 @@
+using UnityEngine;
+
+public class testing : MonoBehaviour
+{
+    public GameObject cube;
+    Transform t;
+    float speed = 0.01f;
+
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        t = cube.GetComponent<Transform>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(t.position.y > 10)
+        {
+            speed = speed * -1;  
+        }else if(t.position.y < -10)
+        {
+            speed = speed * -1;
+        }
+
+        t.Translate(0,speed,0);
+    }
+}
